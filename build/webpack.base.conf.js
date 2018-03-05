5f58c878e107d5ae3fb372ad05d5437e67dd4102
stone <674656681@qq.com>
2018-03-05 18:36:31 +0800

    1
    Signed-off-by: stone <674656681@qq.com>

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -40,8 +40,8 @@ module.exports = {
             },
             {
                 test: /\.js$/,
-                loader: 'babel-loader',
-                include: [resolve('src'), resolve('test')]
+                loader: 'babel-loader', 
+                include: [resolve('src'), resolve('test'), resolve('node_modules/vue-echarts-v3/src')]
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
