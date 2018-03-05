64d2724edd4bda3379e7eb3de5cd441f83532694
冰风 <359446646@qq.com>
2018-03-05 15:35:13 +0800

    111
    Signed-off-by: 冰风 <359446646@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="app"> 
-    <router-view id="router-view"></router-view> 
-  </div>
+<div id="app">
+	<router-view></router-view>
+</div>
 </template>
 <script>
-export default {
-  created() {}
-};
-</script>
  
+export default{
+  
+     
+}
+</script>
+
--- src/router/need.js
+++ src/router/need.js
@@ -1,7 +1,19 @@
 export let router = [
-	{
+{
 		path: '/home',
 		name: 'home',
-		component: r => require.ensure([], () => r(require('@/view/summary/Summary.vue')))
+		component: r => require.ensure([], () => r(require('@/view/summary/Summary.vue'))),
+		children:[
+           /* {
+                path:'/index',
+                component: r => require.ensure([], () => r(require('@/components/summary/WorldMap.vue'))),
+                name:'worldMap'
+            },
+            {
+                path:'world3dmap',
+                component: r => require.ensure([], () => r(require('@/components/summary/worldMap3D.vue'))), 
+                name:'world3dmap'
+            }*/
+        ] 
 	} 
 ]
